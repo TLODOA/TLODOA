6599f37a6292081b8ebb91f6d1f9fe289b613f4c
Cristiano <cristianochincarosanches@gmail.com>
2025-11-07 11:18:24 -0300

    improve user page style and responsiveness

--- static/script/layout/layout_1.js
+++ static/script/layout/layout_1.js
@@ -3,4 +3,12 @@ import * as global from '../globals.js'
 //
 const layout_1 = new global.Layout_1();
 
+/*
+console.log(layout_1.ELEMENTS_BY_TAG);
+console.log(layout_1.ELEMENTS_BY_CLASS);
+console.log(layout_1.ELEMENT_BY_ID);
+
+console.log(layout_1.CLASS_ATTR_DEFAULT);
+*/
+
 layout_1.set_dynamic_classNames();
--- static/script/layout/layout_3.js
+++ static/script/layout/layout_3.js
@@ -2,10 +2,4 @@ import * as global from '../globals.js'
 
 const layout_3 = new global.Layout_3();
 
-// console.log(layout_3.ELEMENTS_BY_TAG);
-// console.log(layout_3.ELEMENTS_BY_CLASS);
-// console.log(layout_3.ELEMENT_BY_ID);
-
-// console.log(layout_3.CLASS_ATTR_DEFAULT);
-
 layout_3.set_dynamic_classNames();
