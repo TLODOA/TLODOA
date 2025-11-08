3fb4eff4bfc24b5416f27a34d45eaceebe26ed94
Cristiano <cristianochincarosanches@gmail.com>
2025-11-08 18:54:54 -0300

    feat user infos for user page

--- routers/routers_user.py
+++ routers/routers_user.py
@@ -8,12 +8,27 @@ def register_app(app:object)->None:
     
     @app.route('/view/user/<user_name>/page')
     def view_user_infos(user_name)->object:
+        import time
+
         try:
             userInfos_wrap = session_query(UserInfos, userName=user_name)[0]
             userInfos = model_unwrap(userInfos_wrap)
-            print('userInfos: ', userInfos)
 
-            return flask.render_template('user_page.html', user=userInfos)
+            time_viewed_last = time.localtime(userInfos["time_viewed_last"])
+            time_arrival = time.localtime(userInfos["time_arrival"])
+
+            user_data = {
+                "userName": userInfos["userName"],
+                "description": userInfos["description"],
+                "photoPath": userInfos["photoPath"],
+
+                "infos": {
+                    "Last connection": f"{ time.strftime("%d/%m/%Y, %H:%M:%S", time_viewed_last) } ",
+                    "Here since": f"{ time.strftime("%A, %B %d, %Y %H:%M:%S GMT%Z", time_arrival) }"
+                }
+            }
+
+            return flask.render_template('user_page.html', user=user_data)
 
         ## Where needs a dedicate error page
         except IndexError as e:
